4c3a351302da6c138002bc7eb84b9b7b811e6f11
Louis <honglouis@outlook.com>
2015-07-28 10:23:19 +0800

    Initial commit!

---
+++ TransformLooper.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TransformLooper : MonoBehaviour {
+
+	public Vector3 Direction = new Vector3(1,0,0);
+	public int TileCount = 1;
+	public float Distance = 1;
+	public float Speed = 1;
+
+	private void Start() {
+		if (TileCount > 1) {
+			SpawnNextClone();
+		}
+		StartLoop();
+	}
+
+	private void SpawnNextClone() {
+		TransformLooper instance = Instantiate(this) as TransformLooper;
+		instance.transform.position = instance.transform.position + Direction * Distance;
+		instance.TileCount--;
+	}
+
+	public void StartLoop() {
+		StartCoroutine(Loop());
+	}
+
+	// Update is called once per frame
+	private IEnumerator Loop () {
+		var wait = new WaitForFixedUpdate();
+		var startPos = transform.position;
+		while (true) {
+			transform.position = GetLoopLerp(startPos, Direction, Distance, Speed);
+			yield return wait;
+		}
+	}
+
+	public static Vector3 GetLoopLerp(Vector3 start, Vector3 direction, float length, float speed) {
+		var timespan = length / speed;
+		var t = Time.time % timespan / timespan;
+		return Vector3.Lerp(start, start + direction * length, t);
+	}
+
+	public void Reset() {
+
+	}
+}
