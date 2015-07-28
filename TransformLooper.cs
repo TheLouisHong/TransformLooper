712fdb59f92b82d89d0981139cf8ca1c6bc83941
Louis <honglouis@outlook.com>
2015-07-29 02:30:43 +0800

    Makes the child tiles become child transform

--- TransformLooper.cs
+++ TransformLooper.cs
@@ -18,7 +18,11 @@ public class TransformLooper : MonoBehaviour {
 
 	private void SpawnNextClone() {
 		TransformLooper instance = Instantiate(this) as TransformLooper;
-		instance.transform.position = instance.transform.position + Direction * Distance;
+		instance.transform.SetParent(transform);
+		instance.transform.position = transform.position + Direction * Distance;
+		instance.transform.localScale = Vector3.one;
+		instance.transform.localRotation = Quaternion.Euler(Vector3.zero);
+		
 		instance.TileCount--;
 	}
 
@@ -26,7 +30,6 @@ public class TransformLooper : MonoBehaviour {
 		StartCoroutine(Loop());
 	}
 
-	// Update is called once per frame
 	private IEnumerator Loop () {
 		var wait = new WaitForFixedUpdate();
 		var startPos = transform.position;
@@ -42,7 +45,4 @@ public class TransformLooper : MonoBehaviour {
 		return Vector3.Lerp(start, start + direction * length, t);
 	}
 
-	public void Reset() {
-
-	}
 }
